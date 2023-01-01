21130e76cd7c0d5d8b7627d2b768a9b17a79df50
Sangeeta Jadoonanan <6550256+sjbitcode@users.noreply.github.com>
2023-01-01 02:50:06 -0500

    remove djdt from test project

--- tests/settings.py
+++ tests/settings.py
@@ -23,7 +23,6 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
-    "debug_toolbar.middleware.DebugToolbarMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
